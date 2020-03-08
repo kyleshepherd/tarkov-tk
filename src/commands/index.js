693e49ff586aaf7ef90373546447ac2f8a44d8ce
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-03-08 16:31:58 +0000

    Message checking and splitting for handler

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,5 +1,8 @@
 module.exports = async (msg) => {
 	if (msg.guild.id === process.env.SERVER_ID) {
-		console.log(msg.content);
+		const message = msg.content.toLowerCase();
+		const args = message.split(' ');
+		if (args.length === 0 || args[0].charAt(0) !== '!') return;
+		console.log(args);
 	}
-}
+};
