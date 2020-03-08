049f9d9a84379caf2cd8d094203ce62399148604
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-03-08 19:19:50 +0000

    Removed log

--- src/commands/start.js
+++ src/commands/start.js
@@ -2,7 +2,7 @@ var client = require('../db_helper');
 
 module.exports = async (msg) => {
 	const tableCheck = 'SELECT 1 from kills LIMIT 1;';
-	client.query(tableCheck, async function (err, result) {
+	client.query(tableCheck, async function (result) {
 		if (result) {
 			//Table exists
 			await msg.channel.send('Tarkov TK has already been setup on this server. Use !help to see how to use Tarkov TK.');
@@ -11,7 +11,6 @@ module.exports = async (msg) => {
 			const createTable = 'CREATE TABLE kills (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, killer VARCHAR(255), victim VARCHAR(255));';
 			client.query(createTable, async function (err) {
 				if (err) throw err;
-				console.log('Table created');
 				await msg.channel.send('Tarkov TK is setup and ready to use. Use !help to see how to use Tarkov TK.');
 			});
 		}
