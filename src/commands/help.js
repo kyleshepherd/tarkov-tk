6ec18a05e08956e5ad279b6a9c04f84bc881711f
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-03-14 09:58:27 +0000

    Completed help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,3 +1,8 @@
 module.exports = async (msg) => {
-	console.log('help');
+	let helpMsg = '**Tarkov TK Help** \n \n';
+	helpMsg += '`!start` - This will initalise the bot, if not already done \n \n';
+	helpMsg += '`!log @Killer @Victim` - This will log a team kill, where the first tagged user is the killer, and the second is the victim \n \n';
+	helpMsg += '`!kills` - This will display a scoreboard of the users with the most team kills \n \n';
+	helpMsg += '`!deaths` - This will display a scoreboard of the users with the most team deaths \n \n';
+	await msg.channel.send(helpMsg);
 };
