19e209dd8babb106d5429ac1a03fca1f65ebf9e3
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-03-15 13:27:12 +0000

    Added reason to !tkhelp

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,7 +1,7 @@
 module.exports = async (msg) => {
 	let helpMsg = '**Tarkov TK Help** \n \n';
 	helpMsg += '`!tkstart` - This will initalise the bot, if not already done \n \n';
-	helpMsg += '`!tklog @Killer @Victim` - This will log a team kill, where the first tagged user is the killer, and the second is the victim \n \n';
+	helpMsg += '`!tklog @Killer @Victim` - This will log a team kill, where the first tagged user is the killer, and the second is the victim.\nYou can also include a reason e.g. `!log @Killer @Victim Killer thought Victim was a Scav` \n \n';
 	helpMsg += '`!tkkills` - This will display a scoreboard of the users with the most team kills \n \n';
 	helpMsg += '`!tkdeaths` - This will display a scoreboard of the users with the most team deaths \n \n';
 	await msg.channel.send(helpMsg);
