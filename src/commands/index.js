763408080d805383fd88b4eecd8cad95943708c8
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-03-15 18:15:42 +0000

    brought back caps for reason

--- src/commands/index.js
+++ src/commands/index.js
@@ -19,9 +19,10 @@ const commands = {
 };
 
 module.exports = async (msg) => {
-	const message = msg.content.toLowerCase();
+	const message = msg.content;
 	const args = message.split(' ');
 	if (args.length === 0 || args[0].charAt(0) !== '!') return;
+	args[0].toLowerCase();
 	const command = args.shift().substr(1);
 	if (Object.keys(commands).includes(command)) {
 		commands[command](msg, args);
