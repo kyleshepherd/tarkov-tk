d42e5aa163469d95c4d49135061c0746a3c551e8
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-03-15 17:59:03 +0000

    Added info command to help

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,5 +4,6 @@ module.exports = async (msg) => {
 	helpMsg += '`!tklog @Killer @Victim` - This will log a team kill, where the first tagged user is the killer, and the second is the victim.\nYou can also include a reason e.g. `!log @Killer @Victim Killer thought Victim was a Scav` \n \n';
 	helpMsg += '`!tkkills` - This will display a scoreboard of the users with the most team kills \n \n';
 	helpMsg += '`!tkdeaths` - This will display a scoreboard of the users with the most team deaths \n \n';
+	helpMsg += '`!tkinfo` - Some info about the project and the creator, Kyle';
 	await msg.channel.send(helpMsg);
 };
