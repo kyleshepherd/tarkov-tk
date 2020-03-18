5ff94b531c7da3c5361cddd07a308b026b979327
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-03-18 18:29:20 +0000

    Added info about remove command to help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,6 +2,7 @@ module.exports = async (msg) => {
 	let helpMsg = '**Tarkov TK Help** \n \n';
 	helpMsg += '`!tkstart` - This will initalise the bot, if not already done \n \n';
 	helpMsg += '`!tklog @Killer @Victim` - This will log a team kill, where the first tagged user is the killer, and the second is the victim.\nYou can also include a reason e.g. `!tklog @Killer @Victim Killer thought Victim was a Scav` \n \n';
+	helpMsg += '`!tkremove` - This will remove the most recently logged kill, in case you log an incorrect kill by accident \n \n';
 	helpMsg += '`!tkkills` - This will display a scoreboard of the users with the most team kills \n \n';
 	helpMsg += '`!tkdeaths` - This will display a scoreboard of the users with the most team deaths \n \n';
 	helpMsg += '`!tkinfo` - Some info about the project and the creator, Kyle';
