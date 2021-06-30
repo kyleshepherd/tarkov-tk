35ef2e1d93627db17ea0c42dac403770de0faa45
Kyle Shepherd <kyle@thisissoon.com>
2021-06-30 16:57:09 +0100

    feat(Info): Add patreon link

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,6 +1,6 @@
 module.exports = async (msg) => {
 	let helpMsg = '**Thanks for using Tarkov TK** \n \n';
 	helpMsg += 'Tarkov TK is very much a work in progress, so if you have any suggestions or issues, please let me know via Twitter https://twitter.com/KyleShepherdDev \n\n';
-	helpMsg += 'Also if you enjoy the bot and want to support me, any help would be appreciated! https://ko-fi.com/kyleshepherd';
+	helpMsg += 'Also if you enjoy the bot and want to support me, any help would be appreciated! https://www.patreon.com/tarkovtk';
 	await msg.channel.send(helpMsg);
 };
