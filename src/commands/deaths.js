b9c92031ca243cd0700a49d0fcd16051af578f22
Kyle Shepherd <kyle@thisissoon.com>
2021-06-30 15:20:45 +0100

    feat(Deaths)_: Rewrote deaths command

--- src/commands/deaths.js
+++ src/commands/deaths.js
@@ -1,22 +1,36 @@
-module.exports = async (msg) => {
-	// var existing = db_checker(msg);
-	// existing.then(async function(result) {
-	// 	if (result) {
-	// 		var players = await get_players(msg);
-	// 		for (let i = 0; i < players.length; i++) {
-	// 			let playerDeaths = await get_player_deaths(players[i].player_id, msg);
-	// 			players[i].deaths = playerDeaths[0].deathCount;
-	// 		}
-	// 		players = sortByKey(players, 'deaths');
+const firebase = require('firebase');
 
-	// 		let deathMsg = '**Most Team Deaths \n**';
-	// 		for (let i = 0; i < players.length; i++) {
-	// 			var playerName = await get_player_name(msg, players[i].player_id);
-	// 			deathMsg += (i + 1) + '. **' + playerName + '** - ' + players[i].deaths + ' TDs \n';
-	// 		}
-	// 		await msg.channel.send(deathMsg);
-	// 	} else {
-	// 		await msg.channel.send('Tarkov TK has not been set up on this server. Run `!tkstart` to do so.');
-	// 	}
-	// });
+module.exports = async (msg) => {
+	firebase.firestore().collection('kills').where('serverId', '==', msg.guild.id).get()
+		.then((killsQuery) => {
+			let kills = [];
+			kills = killsQuery.docs.map(kill => ({
+				victim: kill.data().victim,
+			}));
+			let players = [];
+			players = kills.reduce((unique, o) => {
+				if(!unique.some(obj => obj === o.victim)) {
+					unique.push(o.victim);
+				}
+				return unique;
+			},[]);
+
+			let playerDeaths = [];
+			for (const player of players) {
+				const playerDeathCount = kills.filter(kill => kill.victim === player).length;
+				playerDeaths.push({player: player, killCount: playerDeathCount});
+			}
+			playerDeaths = playerDeaths.sort((a,b) => b.killCount - a.killCount);
+
+
+			let killMsg = '**Most Team Deaths\n**';
+			for (let i = 0; i < playerDeaths.length; i++) {
+				killMsg += (i + 1) + '. **' + msg.guild.member(playerDeaths[i].player).nickname + '** - ' + playerDeaths[i].killCount + ' TDs\n';
+			}
+
+			msg.channel.send(killMsg);
+		})
+		.catch((error) => {
+			console.log('Error getting documents: ', error);
+		});
 };
