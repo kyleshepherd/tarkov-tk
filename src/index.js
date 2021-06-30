6b03fcab0d9ff1c8379f2258a77ddd21e253618c
Kyle Shepherd <kyle@thisissoon.com>
2021-06-30 13:31:58 +0100

    feat(Firebase): Add Firebase init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 const Discord = require('discord.js');
+const firebase = require('firebase');
 
 const commandHandler = require('./commands');
 
 require('dotenv').config();
 
 const client = new Discord.Client();
+
+const firebaseConfig = {
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// client.once('ready', async() => {
+// 	var count = 0;
+// 	client.guilds.cache.forEach(() => {
+// 		count++;
+// 	});
+// 	console.log(count);
+// });
 
 client.on('message', commandHandler);
 
