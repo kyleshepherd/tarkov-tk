fbeee8381fa1899a0b5e5117c1d0e6483fb25108
Kyle Shepherd <kyle@thisissoon.com>
2021-06-30 16:01:08 +0100

    feat(Stats): Rewrote stats command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,6 +1,5 @@
 module.exports = async (msg) => {
 	let helpMsg = '**Tarkov TK Help** \n \n';
-	helpMsg += '`!tkstart` - This will initalise the bot, if not already done \n \n';
 	helpMsg += '`!tklog @Killer @Victim` - This will log a team kill, where the first tagged user is the killer, and the second is the victim.\nYou can also include a reason e.g. `!tklog @Killer @Victim Killer thought Victim was a Scav` \n \n';
 	helpMsg += '`!tkremove` - This will remove the most recently logged kill, in case you log an incorrect kill by accident \n \n';
 	helpMsg += '`!tkreset` - This command will reset the TK server data for your channel. **THIS WILL DELETE ALL TK LOGS** \n \n';
--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,70 +1,77 @@
+const firebase = require('firebase');
+
 module.exports = async (msg, args) => {
 	if (args.length < 1) {
 		// stats for whole server
+		firebase.firestore().collection('kills').where('serverId', '==', msg.guild.id).orderBy('date', 'desc').get()
+			.then((statsQuery) => {
+				let stats = [];
+				stats = statsQuery.docs.map(kill => ({
+					date: kill.data().date,
+					killer: kill.data().killer,
+					victim: kill.data().victim,
+					reason: kill.data().reason,
+				}));
+
+				let statMsg = '**Server Kill Stats**\n';
+				for (let i = 0; i < stats.length; i++) {
+					const killerName = msg.guild.member(stats[i].killer).nickname;
+					const victimName = msg.guild.member(stats[i].victim).nickname;
+					const date = stats[i].date.toDate();
+
+					statMsg += date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' - Killer: **' + killerName + '** - Victim: **' + victimName + '** ';
+					if (stats[i].reason !== '') {
+						statMsg += '- Reason: "' + stats[i].reason + '"';
+					}
+					statMsg += '\n\n';
+				}
+
+				msg.channel.send(statMsg);
+			})
+			.catch((error) => {
+				console.log('Error getting documents: ', error);
+			});
 	} else {
 		// stats for single player
-	}
-	// var existing = db_checker(msg);
-	// existing.then(async function(result) {
-	// 	if (result) {
-	// 		if (args.length < 1) {
-	// 			var sql = 'SELECT * FROM kills_' + msg.guild.id + ' ORDER BY id DESC';
-	// 			db.query(sql, async function(err, result) {
-	// 				if (err) throw err;
-	// 				var statMsg = '**Server Kill Stats** \n \n';
-	// 				for (let i = 0; i < result.length; i++) {
-	// 					var killerName = await get_player_name(msg, result[i].killer);
-	// 					var victimName = await get_player_name(msg, result[i].victim);
-	// 					var date = new Date(result[i].date);
-	// 					date = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-	// 					statMsg += date + ' - Killer: **' + killerName + '** - Victim: **' + victimName + '** ';
-	// 					if (result[i].reason != null) {
-	// 						statMsg += '- Reason: "' + result[i].reason + '"';
-	// 					}
-	// 					statMsg += '\n \n';
-	// 				}
-	// 				await msg.channel.send(statMsg);
-	// 			});
-	// 		} else {
-	// 			if (msg.mentions.users.size < 1) {
-	// 				await msg.channel.send('Make sure you tag a user to see their stats e.g. `!log @Player`');
-	// 			} else {
-	// 				const iterator = msg.mentions.users.values();
-	// 				const player = iterator.next().value;
+		if (msg.mentions.users.size < 1) {
+			await msg.channel.send('Make sure you tag a user to see their stats e.g. `!log @Player`');
+		} else {
+			const iterator = msg.mentions.users.values();
+			const player = iterator.next().value;
 
-	// 				const playerCheck = 'SELECT * FROM players_' + msg.guild.id + ' WHERE player_id = ' + player.id + ';';
-	// 				db.query(playerCheck, async function (err, result) {
-	// 					if (err) throw err;
-	// 					if (result === undefined || result.length == 0) {
-	// 						await msg.channel.send('The tagged player does not exist in the database.');
-	// 					} else {
-	// 						const killQuery = 'SELECT * FROM kills_' + msg.guild.id + ' WHERE killer = ' + player.id + ' ORDER BY id DESC;';
-	// 						db.query(killQuery, async function (err, result) {
-	// 							if (err) throw err;
-	// 							if (result === undefined || result.length == 0) {
-	// 								await msg.channel.send('<@' + player.id + '> hasn\'t team killed anyone...yet');
-	// 							} else {
-	// 								var playerName = await get_player_name(msg, player.id)
-	// 								var statMsg = '**' + playerName + ' Team Kills:** \n \n';
-	// 								for (let i = 0; i < result.length; i++) {
-	// 									var victimName = await get_player_name(msg, result[i].victim);
-	// 									var date = new Date(result[i].date);
-	// 									date = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-	// 									statMsg += date + ' - Victim: **' + victimName + '** ';
-	// 									if (result[i].reason != null) {
-	// 										statMsg += '- Reason: "' + result[i].reason + '"';
-	// 									}
-	// 									statMsg += '\n \n';
-	// 								}
-	// 								await msg.channel.send(statMsg);
-	// 							}
-	// 						});
-	// 					}
-	// 				});
-	// 			}
-	// 		}
-	// 	} else {
-	// 		await msg.channel.send('Tarkov TK has not been set up on this server. Run `!tkstart` to do so.');
-	// 	}
-	// });
+			firebase.firestore().collection('kills').where('serverId', '==', msg.guild.id).where('killer', '==', player.id).orderBy('date', 'desc').get()
+				.then((statsQuery) => {
+					let stats = [];
+					stats = statsQuery.docs.map(kill => ({
+						date: kill.data().date,
+						victim: kill.data().victim,
+						reason: kill.data().reason,
+					}));
+
+					const killerName = msg.guild.member(player.id).nickname;
+
+					if (stats.length === 0) {
+						msg.channel.send(killerName + ' hasn\'t team killed anyone...yet');
+					} else {
+						let statMsg = '**' + killerName + ' Team Kills:** \n \n';
+
+						for (let i = 0; i < stats.length; i++) {
+							const victimName = msg.guild.member(stats[i].victim).nickname;
+							const date = stats[i].date.toDate();
+
+							statMsg += date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()  + ' - Victim: **' + victimName + '** ';
+							if (stats[i].reason !== '') {
+								statMsg += '- Reason: "' + stats[i].reason + '"';
+							}
+							statMsg += '\n \n';
+
+						}
+						msg.channel.send(statMsg);
+					}
+				})
+				.catch((error) => {
+					console.log('Error getting documents: ', error);
+				});
+		}
+	}
 };
