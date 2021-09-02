bed0a61fae082e62ce1bdd2a7fd5fb8f370f1b05
Kyle Shepherd <kyle@thisissoon.com>
2021-09-02 19:10:16 +0100

    feat(Stats): Fix overflow char error

--- src/index.js
+++ src/index.js
@@ -1,37 +1,41 @@
-const Discord = require('discord.js');
-const firebase = require('firebase/app');
-require('firebase/firestore');
+const Discord = require("discord.js");
+const firebase = require("firebase/app");
+require("firebase/firestore");
 
-const commandHandler = require('./commands');
+const commandHandler = require("./commands");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const client = new Discord.Client();
-const disbut = require('discord-buttons');
+const disbut = require("discord-buttons");
 disbut(client);
 
 const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-client.on('message', commandHandler);
+client.on("message", commandHandler);
 
-client.on('clickButton', async (button) => {
-	await firebase.firestore().collection('kills').doc(button.id).delete()
-		.then(() => {
-			button.reply.send('Kill removed');
-			button.message.delete();
-		})
-		.catch((error) => {
-			console.error('Error removing kill: ', error);
-		});
+client.on("clickButton", async (button) => {
+  await firebase
+    .firestore()
+    .collection("kills")
+    .doc(button.id)
+    .delete()
+    .then(() => {
+      button.reply.send("Kill removed");
+      button.message.delete();
+    })
+    .catch((error) => {
+      console.error("Error removing kill: ", error);
+    });
 });
 
 client.login(process.env.BOT_TOKEN);
